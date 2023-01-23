a1e288105580559a8757f825b0f836b4f667622d
LvDanyu <763159242@qq.com>
2023-01-23 15:53:42 +0800

    test

---
+++ components/Experience.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Experience({}: Props) {
+  return (
+    <div className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row
+    max-w-full px-10 justify-evenly mx-auto items-center">
+      <h3>Experience</h3>
+    </div>
+  )
+}
